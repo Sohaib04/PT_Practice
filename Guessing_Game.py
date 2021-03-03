f81f5cfed14dd845ed38431cd41f87fe13556b6a
Sohaib Butt <71372925+Sohaib04@users.noreply.github.com>
2021-03-03 23:08:44 +0200

    Guessing_Game.py (Comments)

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -6,7 +6,7 @@ hidden_word = random.choice(words)
 clues = list('????????')
 
 
-def update_clue(guessed_letter, hidden_word, clues):
+def update_clue(guessed_letter, hidden_word, clues): #Changes the '?' in the list to the letter guessed by the user
     index = 0
     while index < len(hidden_word):
         if guessed_letter == hidden_word[index]:
@@ -19,11 +19,11 @@ while lives > 0:
     print('Lives left: ' + str(lives))
     guess = input('Enter a letter or the whole word: ')
 
-    if guess == hidden_word:
+    if guess == hidden_word: #When the user guesses the whole word
         print("You've won! The hidden word was " + hidden_word)
         break
 
-    if guess in hidden_word:
+    if guess in hidden_word: #When the user enters a letter
         update_clue(guess, hidden_word, clues)
     else:
         print('Incorrect! You lost a life. Try again.')
