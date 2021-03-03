048c0286ff2375c84cf25aeffa30debe6cd46032
Sohaib <sohaib_04@pc-8.home>
2021-03-03 23:01:19 +0200

    Initial commit

---
+++ Guessing_Game.py
@@ -0,0 +1,34 @@
+import random
+
+lives = 5
+words = ['airplane', 'scissors', 'treasure', 'syllable', 'magnolia', 'strength', 'business']
+hidden_word = random.choice(words)
+clues = list('????????')
+
+
+def update_clue(guessed_letter, hidden_word, clues):
+    index = 0
+    while index < len(hidden_word):
+        if guessed_letter == hidden_word[index]:
+            clues[index] = guessed_letter
+        index = index + 1
+
+
+while lives > 0:
+    print(clues)
+    print('Lives left: ' + str(lives))
+    guess = input('Enter a letter or the whole word: ')
+
+    if guess == hidden_word:
+        print("You've won! The hidden word was " + hidden_word)
+        break
+
+    if guess in hidden_word:
+        update_clue(guess, hidden_word, clues)
+    else:
+        print('Incorrect! You lost a life. Try again.')
+        lives = lives - 1
+
+    if lives == 0:
+        print("Tough luck! The hidden word was " + hidden_word)
+        break
